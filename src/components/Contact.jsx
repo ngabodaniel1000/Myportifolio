6a7057fdecc6dd3493f40628cb113d1c1c731f96
ngabodaniel1000 <ngabodaniel1000@gmail.com>
2025-01-13 10:37:58 +0200

    contact us ep1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
 
 function Contact() {
+  const handlesubmit = ()=>{
+
+  }
   return (
-    <div className='min-h-screen bg-yellow-300 pt-[150px] lg:pt-[100px]'>Contact</div>
+    <div className=' min-h-screen pt-[150px] lg:pt-[100px]'>
+    <h1 className='text-center pt-5 text-2xl font-bold text-white mb-10'>Contact us</h1>
+    <p className='text-white text-center mb-10'>We value your feedback and are here to assist you with any questions or concerns. <br /> Feel free to reach out to us through our email at ngabodaniel1000@gmail.com or call us at +250 734196604</p>
+      <div className="contact flex gap-32">
+      <div className='flex flex-col lg:ml-10 lg:w-[40%] w-full bg-yellow-300'>
+      <div className='flex flex-row'>
+      <div className='flex flex-col'>
+        <p>Address</p>
+        <p>Rwanda Rubavu,Rugerero</p>
+      </div>
+      </div>
+      <div className='flex flex-row'>
+      <div className='flex flex-col'>
+        <p>Phone</p>
+        <p>+250 734196604</p>
+      </div>
+      </div>
+      <div className='flex flex-row'>
+      <div className='flex flex-col'>
+        <p>Email</p>
+        <p>ngabodaniel1000@gmail.com</p>
+      </div>
+      </div>
+      </div>
+      <div className='lg:w-[40%] w-full'>
+      <form onSubmit={handlesubmit} className='flex flex-col h-[500px] bg-[#333] p-10 rounded-2xl text-lg gap-4 text-white'>
+      <label htmlFor="username">Username</label>
+      <input type="text" name='username' className='outline-none h-[50px] text-[#333]'/>
+      <label htmlFor="email">Email</label>
+      <input type="email" name='email' className='outline-none h-[50px] text-[#333]'/>
+      <label htmlFor="message">Message</label>
+      <textarea name="message" cols={20} rows={5} className='outline-none text-[#333]'></textarea>
+      <input type="submit" className='bg-blue-600 p-4 rounded-2xl'/>
+      </form>
+      </div>
+      </div>
+      
+    </div>
   )
 }
 
--- src/components/Testomonials.jsx
+++ src/components/Testomonials.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import Elysee from "../assets/images/elysee.jpg"
+import Fils from "../assets/images/aimefils.jpg"
+import Hirwamj from "../assets/images/profile.jpg"
+import Enock from "../assets/images/enock.jpg"
+import Mom from "../assets/images/mum.png"
+import Friend from "../assets/images/Friend.jpg"
+const testimonials=[
+  {
+  name: "Mukunzi elyseee",
+  description: "I am teacher of ngabo daniel \n He is hardworking student good at all \n either school course or programming",
+  imageurl: Elysee,
+},
+  {
+  name: "Byiringiro aime fils",
+  description: "I am co-worker and classmate of ngabo daniel \n He can achieve everything he set as goal",
+  imageurl: Fils,
+},
+  {
+  name: "Hirwa mj",
+  description: "I am brother of Ngabo daniel \n He is pussy coder on the code he is the fellow ghost",
+  imageurl: Hirwamj,
+},
+  {
+  name: "Dusabe enock",
+  description: "I am friend of ngabo daniel \n He appreciate and support everyone ideas\n",
+  imageurl: Enock,
+},
+  {
+  name: "Jay kay",
+  description: "I am his friend and ex-classmate of ngabo daniel \n He had vision in his life he was honest and patient ",
+  imageurl: Friend,
+},
+  {
+  name: "Mom",
+  description: "Ngabo daniel is my fellow boy he respect elders and he always know wht to do in life",
+  imageurl: Mom,
+},
+]
 
 function Testomonials() {
   return (
-    <div className='min-h-screen bg-red-800 pt-[150px] lg:pt-[100px]'>Testomonials</div>
+    <div className='min-h-screen pt-[150px] lg:pt-[100px]'>
+      <h1 className='text-2xl text-white text-center font-bold'>Testimonials</h1>
+      <div className='w-[90%] ml-4 lg:ml-[100px] mt-10 grid grid-cols-1 md:grid-cols-3'>
+        {testimonials.map((testimonial)=>
+        <div className="flex flex-col gap-5 text-white bg-[#333] rounded-xl  w-[400px] h-[300px] mt-4">
+        <img src={testimonial.imageurl} alt="Images" className='w-20 h-20 rounded-full mt-2 ml-40'/>
+        <p className='text-xl ml-[80px]'>{testimonial.name}</p>
+        <q className='ml-[80px]'>{testimonial.description}</q>
+        </div>
+        )}
+      </div>
+    </div>
   )
 }
 
